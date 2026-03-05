b21421ef0337c7a85df38b1707b2ea67cd91ac96
Skylark <37351525+SleepyLark@users.noreply.github.com>
2026-03-05 12:22:40 -0700

    initial commit

---
+++ ChessBrowser/Components/Class.cs
@@ -0,0 +1,125 @@
+﻿namespace ChessBrowser.Components
+{
+    public class ChessGame
+    {
+        private string eventName;
+        private string site;
+        private DateTime date;
+        private float round;
+        private string whitePlayer;
+        private string blackPlayer;
+        private string result;
+        private string whiteElo;
+        private string blackElo;
+        private string ECO;
+        private DateTime eventDate;
+        private string moves;
+
+        public ChessGame(string eventName, string site, DateTime date, float round,
+        string whitePlayer, string blackPlayer, string result, string whiteElo,
+        string blackElo, string ECO, DateTime eventDate, string moves)
+        {
+            this.eventName = eventName;
+            this.site = site;
+            this.date = date;
+            this.round = round;
+            this.whitePlayer = whitePlayer;
+            this.blackPlayer = blackPlayer;
+            this.result = result;
+            this.whiteElo = whiteElo;
+            this.blackElo = blackElo;
+            this.ECO = ECO;
+            this.eventDate = eventDate;
+            this.moves = moves;
+        }
+
+        public ChessGame()
+        {
+
+        }
+
+        // Event Name
+        public string EventName
+        {
+            get { return eventName; }
+            set { eventName = value; }
+        }
+
+        // Site
+        public string Site
+        {
+            get { return site; }
+            set { site = value; }
+        }
+
+        // Date
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        // Round
+        public float Round
+        {
+            get { return round; }
+            set { round = value; }
+        }
+
+        // White Player
+        public string WhitePlayer
+        {
+            get { return whitePlayer; }
+            set { whitePlayer = value; }
+        }
+
+        // Black Player
+        public string BlackPlayer
+        {
+            get { return blackPlayer; }
+            set { blackPlayer = value; }
+        }
+
+        // Result
+        public string Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        // White Elo
+        public string WhiteElo
+        {
+            get { return whiteElo; }
+            set { whiteElo = value; }
+        }
+
+        // Black Elo
+        public string BlackElo
+        {
+            get { return blackElo; }
+            set { blackElo = value; }
+        }
+
+        // ECO
+        public string EcoCode
+        {
+            get { return ECO; }
+            set { ECO = value; }
+        }
+
+        // Event Date
+        public DateTime EventDate
+        {
+            get { return eventDate; }
+            set { eventDate = value; }
+        }
+
+        // Moves List
+        public string Moves
+        {
+            get { return moves; }
+            set { moves = value; }
+        }
+    }
+}
---
+++ ChessBrowser/Components/PGNParser.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+
+namespace ChessBrowser.Components
+{
+
+    public static class PGNParser
+    {
+
+        public static List<ChessGame> parseFileLine(string[] stream)
+        {
+            List<ChessGame> loadedChessGames = new();
+            ChessGame currentGame = new();
+
+            bool parseMoves = false;
+
+            foreach (string line in stream)
+            {
+                if (line.StartsWith("["))
+                {
+                    int openQuote = line.IndexOf('"')+1;
+                    int closeQuote = line.LastIndexOf('"');
+                    string param = line[openQuote..closeQuote];
+
+                    if (line.StartsWith("[Event"))
+                    {
+                        currentGame.EventName = param;
+                    }
+                    else if (line.StartsWith("[Site"))
+                    {
+                        currentGame.Site = param;
+                    }
+                    else if(line.StartsWith("[Date"))
+                    {
+                        currentGame.EventDate = DateTime.Parse(param);
+                    }
+                    else if (line.StartsWith("[Round"))
+                    {
+                        currentGame.Round = float.Parse(param);
+                    }
+                    else if (line.StartsWith("[White "))
+                    {
+                        currentGame.WhitePlayer = param;
+                    }
+                    else if (line.StartsWith("[Black "))
+                    {
+                        currentGame.BlackPlayer = param;
+                    }
+                    else if (line.StartsWith("[WhiteElo"))
+                    {
+                        currentGame.WhiteElo = param;
+                    }
+                    else if (line.StartsWith("[BlackElo"))
+                    {
+                        currentGame.BlackElo = param;
+                    }
+                    else if (line.StartsWith("[Result"))
+                    {
+                        currentGame.Result = param;
+                    }
+                    else if (line.StartsWith("[ECO"))
+                    {
+                        currentGame.EcoCode = param;
+                    }
+                    else if (line.StartsWith("[EventDate"))
+                    {
+                        currentGame.EventDate = DateTime.Parse(param);
+                    }
+                }
+                else if (line.StartsWith("1."))
+                {
+                    parseMoves = true;
+                    currentGame.Moves += line;
+                }
+                else if (parseMoves && string.IsNullOrEmpty(line))
+                {
+                    parseMoves = false;
+                    loadedChessGames.Add(currentGame);
+
+                    currentGame = new();
+                }
+                else if (parseMoves)
+                {
+                    currentGame.Moves += line;
+                }
+                
+            }
+
+            return loadedChessGames;
+        }
+    }
+
+}
